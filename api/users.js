49f76ee8c3ee7f92b05a9145c3ffb8932f773c49
Natoshia Dumont <natoshia.dumont@gmail.com>
2023-03-01 20:04:01 -0600

    api > users drafted

--- api/users.js
+++ api/users.js
@@ -1,7 +1,87 @@
-//registerUser: POST api/users/register
+/*
+POST api/users/register
+parameters, (username, password)
+should do the following:
+- returns user object (id, username)
+- returns message, the success message
+- sets 'userToken' into local storage
+*/
 
-//loginUser: POST api/users/login
-
-//
 
 
+/*
+POST api/users/login
+parameters, (username, password)
+should do the following:
+-returns user object (id, username)
+-returns message, the success message
+-sets 'userToken' into local storage
+*/
+
+/*
+GET /api/users/me
+NO PARAMETERS
+returns user object(id, username)
+*/
+
+/*
+GET /api/users/:username/orders
+current user's username must match customer_id
+returns array of objects (orders)
+EXAMPLE:
+[
+    {
+      id:3,
+      customer_id: 4,
+      date: '2022-05-05 07:00:00',
+      billing_address: '321 Example Ct.',
+      shipping_address: '321 Example Ct.',
+      total: 200,
+      payment_type: 'Klarna',
+      fulfilled: true
+      order_items: [
+        {
+          customer_id: 4,
+          flavor_id: 3,
+          order_id: 3,
+          quantity: 3
+        },
+        {
+          customer_id: 4,
+          flavor_id: 1,
+          order_id: 3,
+          quantity: 1
+      }
+        ]
+    }
+]
+*/
+
+//GET /api/users/:username/reviews
+
+/*
+
+GET /api/users/:username/reviews
+current user's username must match customer_id
+returns array of objects of reviews created by the user (reviews) 
+EXAMPLE:
+[
+  {
+    id:2,
+    username: 'bobthesnob',
+    reviews: [
+        {
+        flavor_id: 1,
+        author_id: 2,
+        title: "Chocolate rules",
+        content: "Meh.",
+      },
+      {
+        flavor_id: 2,
+        author_id: 2,
+        title: "Vanilla is okay",
+        content: "I can not see the vanilla bean! What gross factory did this come from? I need answers",
+      }]
+  }
+]
+*/
