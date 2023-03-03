477ad9c3874de631751f861d78f1431024ffd59c
Natoshia Dumont <natoshia.dumont@gmail.com>
2023-03-02 18:59:47 -0600

    working on login functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Link, Route, Switch, useHistory } from 'react-router-dom'
-import { Login} from './components/index';
-import { fetchUser } from './api/users';
+// import { Login} from './components/index';
+import {  } from './api/users';
 
 const App = () => {
   const [token, setToken] = useState('');
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
   let history = useHistory();
   console.log(user);
   useEffect(() => {
       setToken(localStorage.getItem('userToken'));
-      fetchUser().then((result) => { setUser(result) });
+      // fetchUser().then((result) => { setUser(result) });
   }, [])
 
   useEffect(() => {
