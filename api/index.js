a63db01f83663f3de82f0408da7bc94f89220759
Sandeep Sunny Nair <sunnynair54@yahoo.com>
2023-03-06 20:47:01 -0600

    updating routes with Postman

--- api/index.js
+++ api/index.js
@@ -7,49 +7,49 @@ const { JWT_SECRET } = process.env;
 
 // 'test push'
 
-router.use(async (req, res, next) => {
-  const prefix = 'Bearer ';
-  const auth = req.header('Authorization');
-  // console.log('middleware called');
-  if (!auth) { // nothing to see here
-    next();
-  } else if (auth.startsWith(prefix)) {
-    const token = auth.slice(prefix.length);
-    // console.log(token);
-    try {
-      const { id } = jwt.verify(token, JWT_SECRET);
-      // console.log(id);
-      if (id) {
-        req.user = await getUserById(id);
-        // console.log(req.user);
-        next();
-      }
-    } catch ({ name, message }) {
-      next({ name, message });
-    }
-  } else {
-    next({
-      name: 'AuthorizationHeaderError',
-      message: `Authorization token must start with ${ prefix }`
-    });
-  }
-  });
-  
+// router.use(async (req, res, next) => {
+//   const prefix = 'Bearer ';
+//   const auth = req.header('Authorization');
+//   // console.log('middleware called');
+//   if (!auth) { // nothing to see here
+//     next();
+//   } else if (auth.startsWith(prefix)) {
+//     const token = auth.slice(prefix.length);
+//     // console.log(token);
+//     try {
+//       const { id } = jwt.verify(token, JWT_SECRET);
+//       // console.log(id);
+//       if (id) {
+//         req.user = await getUserById(id);
+//         // console.log(req.user);
+//         next();
+//       }
+//     } catch ({ name, message }) {
+//       next({ name, message });
+//     }
+//   } else {
+//     next({
+//       name: 'AuthorizationHeaderError',
+//       message: `Authorization token must start with ${prefix}`
+//     });
+//   }
+// });
 
-router.use((req, res, next) => {
-      if (req.user) {
-        console.log("User is set:", req.user);
-      }
-    
-      next();
-    });
+
+// router.use((req, res, next) => {
+//   if (req.user) {
+//     console.log("User is set:", req.user);
+//   }
+
+//   next();
+// });
 
 
 // GET /api/health
 router.get('/health', async (req, res) => {
-    res.send({
-        message: "All is good."
-    })
+  res.send({
+    message: "All is good."
+  })
 });
 
 // ROUTER: /api/users
@@ -58,23 +58,23 @@ router.use('/users', usersRouter);
 
 // ROUTER: /api/activities
 const flavorsRouter = require('./flavors');
-router.use('/activities', flavorsRouter);
+router.use('/flavors', flavorsRouter);
 
-// ROUTER: /api/routines
-const reviewsRouter = require('./reviews');
-router.use('/routines', reviewsRouter);
+// // ROUTER: /api/routines
+// const reviewsRouter = require('./reviews');
+// router.use('/reviews', reviewsRouter);
 
-// ROUTER: /api/routine_activities
-const ordersRouter = require('./orders');
-router.use('/orders', ordersRouter);
+// // ROUTER: /api/routine_activities
+// const ordersRouter = require('./orders');
+// router.use('/orders', ordersRouter);
 
-const orderItemsRouter = require('./order_items');
-router.use('/order_items', orderItemsRouter);
+// const orderItemsRouter = require('./order_items');
+// router.use('/order_items', orderItemsRouter);
 
 //general error handler
 router.use((error, req, res, next) => {
-    res.send(error);
-  });
+  res.send(error);
+});
 
 module.exports = router;
 //test push
--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var app = express()
 
 const morgan = require('morgan');
 
+const { client } = require('./db');
+const PORT = process.env["PORT"] || 5000;
+client.connect();
 // Setup your Middleware and API Router here
 
 app.use(morgan('dev'));
@@ -13,19 +16,25 @@ app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<_____Body Logger END_____>");
-  
-    next();
-  });
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
 
 
-// const router = require('./api');
-// app.use('/api', router);
+const router = require('./api');
+app.use('/api', router);
 
-const { client } = require('./db');
-client.connect();
 
+
+app.listen(PORT, () => {
+  console.log("Server is listening on PORT:", PORT, "Get your Ice Cream on!");
+});
 
 module.exports = app;
+
+
+
+
