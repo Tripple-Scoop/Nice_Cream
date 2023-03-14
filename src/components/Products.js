d609939b8efcd5ad0fb69577db1317fe903aaa9a
Natoshia Dumont <natoshia.dumont@gmail.com>
2023-03-13 23:58:00 -0500

    Minor style updating

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { fetchAllFlavors } from "../api/flavors";
 
 
-const Products = () => {
+const Products = ({user}) => {
   const [flavors, setFlavors] = useState([]);
   const navigate = useNavigate();
 
@@ -22,7 +22,7 @@ const Products = () => {
   }, []);
 
   return (
-    <div className="products-container">
+    <div id="products-container">
       <div className="flavorTitle">
         <h1>Our Flavors</h1>
       </div>
@@ -32,16 +32,23 @@ const Products = () => {
           event.preventDefault();
         }}
       >
-        <div className="flavorBody">
+        <div id="flavor_body">
           {flavors.map((flavor) => {
             return (
               <div className="flavor_info">
-                <div className="flavor_name">{flavor.name}</div>
+                <div className="flavor_name"><h2>{flavor.name}</h2></div>
                 <div className="flavor_type">{flavor.type}</div>
                 <div> <img className="flavor_image" src={flavor.image_url} /></div>
-                <div className="flavor_Description"> Description:{flavor.description}</div>
-                <div className="flavor_Price"> Price: ${flavor.price}</div>
-                <button className="createButton" onClick={() => addFlavorToCart(flavor)}>Add to Cart!</button>
+                <div className="flavor_description"> <p>Description:{flavor.description}</p></div>
+                <div className="flavor_price"> Price: ${flavor.price}</div>
+                <div id="product_options">
+                  <button className="create-button" onClick={() => addFlavorToCart(flavor)}>Add to Cart!</button>
+                  {user.admin ===  true ? (
+                    <div>You are an admin</div>
+                  ) : <div>You are not an admin</div>}
+
+                </div>
+                
               </div>
             );
           })}
