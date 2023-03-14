651f45a62cb59732f79f2ab94164c08ba5291b25
TahjD <tahjjobs@gmail.com>
2023-03-13 18:53:03 -0700

    Cart 80% Done

--- db/order_items.js
+++ db/order_items.js
@@ -1,112 +1,116 @@
+//TAHJ//
 const client = require("./client");
 
 //addToCart(customer_id, flavor_id, quantity)- returns cart item object
 async function addToCart({ customer_id, flavor_id, quantity, order_id }) {
-
-    try {
-        const { rows: [order_item] } = await client.query(
-            `INSERT INTO order_items(customer_id, flavor_id, quantity, order_id)
+  try {
+    const {
+      rows: [order_item],
+    } = await client.query(
+      `INSERT INTO order_items(customer_id, flavor_id, quantity, order_id)
          VALUES ($1, $2, $3, $4)
          RETURNING *;`,
-            [customer_id, flavor_id, quantity, order_id]
-        );
-        return order_item;
-
-    } catch (error) {
-        console.error(`Error adding item to cart!`);
-        throw error;
-    }
+      [customer_id, flavor_id, quantity, order_id]
+    );
+    return order_item;
+  } catch (error) {
+    console.error(`Error adding item to cart!`);
+    throw error;
+  }
 }
 
 //updateQuantity(cart_item.id, newQuant)- allow to quantity of flavor
 async function updateQuantity(id, newQuant) {
-    try {
-        const { rows: [newQuantity] } = await client.query(
-            `UPDATE order_items
+  try {
+    const {
+      rows: [newQuantity],
+    } = await client.query(
+      `UPDATE order_items
        SET quanity = $1
        WHERE id = $2
        RETURNING *;
       `,
-            [newQuant, id]
-        );
-        return newQuantity;
-    } catch (error) {
-        console.error(`Error updating quantity ${id}!`, error);
-    }
+      [newQuant, id]
+    );
+    return newQuantity;
+  } catch (error) {
+    console.error(`Error updating quantity ${id}!`, error);
+  }
 }
-//tahj
+
 //getItemById(cart_item_id)- return cart_item based on it's id
 async function getItemById(id) {
-    try {
-        const { rows: [item] } = await client.query(
-            `SELECT *
+  try {
+    const {
+      rows: [item],
+    } = await client.query(
+      `SELECT *
          FROM order_items
          WHERE id = $1;
         `,
-            [id]
-        );
-        return item;
-    } catch (error) {
-        console.error(`Error retrieving item with id ${id}!`, error);
-    }
+      [id]
+    );
+    return item;
+  } catch (error) {
+    console.error(`Error retrieving item with id ${id}!`, error);
+  }
 }
 
 //getActiveCartItems(customer_id)- return array of cart_items in customer's unsubmitted order
 async function getActiveCartItems(customer_id) {
-    try {
-        const { rows: activeCart } = await client.query(
-            `SELECT *
+  try {
+    const { rows: activeCart } = await client.query(
+      `SELECT *
          FROM order_items
          WHERE customer_id = $1
          AND order_id IS NULL
         `,
-            [customer_id]
-        );
-        return activeCart;
-    } catch (error) {
-        console.error(`Error retrieving cart with id ${customer_id}!`, error);
-    }
+      [customer_id]
+    );
+    return activeCart;
+  } catch (error) {
+    console.error(`Error retrieving cart with id ${customer_id}!`, error);
+  }
 }
-
 
 //getItemsByOrderId(order_id)- return array of cart_items for given order_id
 async function getItemsByOrderId(order_id) {
-    try {
-        const { rows: orders } = await client.query(
-            `SELECT *
+  try {
+    const { rows: orders } = await client.query(
+      `SELECT *
          FROM order_items
          WHERE order_id = $1;
         `,
-            [order_id]
-        );
-        return orders;
-    } catch (error) {
-        console.error(`Error retrieving order with id ${order_id}!`, error);
-    }
+      [order_id]
+    );
+    return orders;
+  } catch (error) {
+    console.error(`Error retrieving order with id ${order_id}!`, error);
+  }
 }
 
 //removeFromCart(id, user_id)
 async function removeFromCart(id, customer_id) {
-    try {
-        const { rows: updatedCart } = await client.query(
-            `DELETE FROM order_items
+  try {
+    const { rows: updatedCart } = await client.query(
+      `DELETE FROM order_items
          WHERE id = $1
 AND customer_id =$2
          RETURNING *;
         `,
-            [id, customer_id]
-        );
-        return updatedCart;
-    } catch (error) {
-        console.error(`Error deleting item with id ${id}!`, error);
-    }
+      [id, customer_id]
+    );
+    return updatedCart;
+  } catch (error) {
+    console.error(`Error deleting item with id ${id}!`, error);
+  }
 }
 
 module.exports = {
-    addToCart,
-    updateQuantity,
-    getItemById,
-    getActiveCartItems,
-    getItemsByOrderId,
-    removeFromCart
-}
+  addToCart,
+  updateQuantity,
+  getItemById,
+  getActiveCartItems,
+  getItemsByOrderId,
+  removeFromCart,
+};
--- src/components/My_Cart.js
+++ src/components/My_Cart.js
@@ -1,21 +1,73 @@
+//TAHJ
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login } from "../api/users";
+import "../components/MyCart.css";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { GiNunchaku } from "react-icons/gi";
+import { Products } from ".";
 
-
-//NATOSHIA
-
-const My_Cart = ({ user, token }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirm, setConfirm] = useState("");
-  const navigate = useNavigate();
-
+const My_Cart = ({
+  shown,
+  flavors,
+  removeFromCart,
+  updateQuantity,
+  onClose,
+}) => {
   return (
-    <div> My Cart</div>
+    <div className="modal" style={{ display: shown ? "block" : "none" }}>
+      <div className="shoppingCart">
+        <div className="header">
+          <h2> Cart </h2>
+          <button className="btn-close" onClick={onClose}>
+            <AiFillCloseCircle size={30}></AiFillCloseCircle>
+          </button>
+        </div>
+        <div className="cart-items">
+          {flavors.length === 0 && (
+            <span className="empty-text">Your Cart Is Empty</span>
+          )}
+          {flavors.map((flavor) => (
+            <div className="cart-item" key={flavor.id}>
+              <img src={flavor.image_url} alt={flavor.name}></img>
+              <div className="flavor-info">
+                <h3>{flavor.name}</h3>
+                <span className="flavor-price">
+                  {flavor.price * flavor.count}
+                </span>
+                <select
+                  className="count"
+                  value={flavor.count}
+                  onChange={(e) => {
+                    updateQuantity(flavor.id, e.target.value);
+                  }}
+                >
+                  {[...Array(10).keys()].map((number) => {
+                    const num = number + 1;
+                    return (
+                      <option value={num} key={num}>
+                        {num}
+                      </option>
+                    );
+                  })}
+                </select>
+                <button
+                  className="btn remove-cart"
+                  onClick={() => removeFromCart(flavor)}
+                >
+                  <RiDeleteBin6Line size={20}></RiDeleteBin6Line>
+                </button>
+              </div>
+            </div>
+          ))}
+          {flavors.length > 0 && (
+            <buttton className="checkout-btn">Proceed to Checkout</buttton>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
-
-
 
 export default My_Cart;
