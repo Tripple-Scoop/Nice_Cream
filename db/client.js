a7ae2dfe9c107f423a52654e960901e1745339ef
Natoshia Dumont <natoshia.dumont@gmail.com>
2023-03-22 20:42:56 -0500

    client.js debug

--- db/client.js
+++ db/client.js
@@ -5,6 +5,6 @@ require('dotenv').config();
 const { DATABASE_URL } = process.env;
 console.log(DATABASE_URL)
 const client = new Client(DATABASE_URL);
-
+console.log(client);
 module.exports = client;
 
